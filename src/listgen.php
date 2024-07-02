d431c8b7d544e0f17406262a216aa623f2b63625
ss21074 <wildsan-21455@outlook.jp>
2024-07-02 13:45:09 +0900

    listgen.php

--- src/listgen.php
+++ src/listgen.php
@@ -22,34 +22,34 @@
             <input type="submit" value="送信">
 		    <input type="reset" value="クリア">
         </form>
+
+            <?php
+            $F = $_POST['F'];
+            $E = $_POST['E'];
+            $S = $_POST['S'];
+            $Pre = $_POST['Pre'];
+            $Post = $_POST['Post'];
+
+            if($F == ""){
+                $F = 0;
+            }
+            if($S == ""){
+                $S = 1;
+            }
+
+            if(is_numeric($F) && is_numeric($E) && is_numeric($S) && $Pre != "" && $Post != ""){
+                for($i = $F; $i < $E; $i = $i + $S){
+                    print $Pre;
+                    print $i;
+                    print $Post;
+                }
+            }
+            else{
+                print "エラー";
+            }
+            ?>
     </body>
 </html>
 
-<?php
-$F = $_POST['F'];
-$E = $_POST['E'];
-$S = $_POST['S'];
-$Pre = $_POST['Pre'];
-$Post = $_POST['Post'];
-
-if($F == ""){
-    $F = 0;
-}
-if($S == ""){
-    $S = 0;
-}
-
-if(is_numeric($F) && is_numeric($E) && is_numeric($S) && $Pre != "" && $Post != ""){
-    for($i = $F; $i < $E; $i = $i + $S){
-        print $Pre;
-        print $i;
-        print $Post;
-    }
-}
-else{
-    print "エラー";
-}
-
-?>
 
 
