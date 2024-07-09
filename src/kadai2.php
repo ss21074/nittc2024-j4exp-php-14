e19be12913cfb2fe1dc1b6632e0e6e30d73c1f2a
ss21074 <wildsan-21455@outlook.jp>
2024-07-09 19:29:07 +0900

    kadai2.php

--- src/kadai2.php
+++ src/kadai2.php
@@ -43,37 +43,43 @@
                 catch (PDOException $e) {
                     echo "Connection failed: " . $e->getMessage();
                 }
+                try{
+                    $ユーザ名 = $_POST['ユーザ名'];
+                    $レポジトリ名 = $_POST['レポジトリ名'];
+                    $イシュータイトル = $_POST['イシュータイトル'];
+                    $ラベル = $_POST['ラベル'];
+                    $優先順位 = $_POST['優先順位'];
+                    $担当者 = $_POST['担当者'];
+                    $イシューコミットID = $_POST['イシューコミットID'];
+
+
+                    $stmt = $pdo->prepare("INSERT INTO issues (title,label,priority,issue_commit) VALUES (:イシュータイトル, :ラベル, :優先順位, :イシューコミットID)");
+                    
+                    $stmt->bindParam(':イシュータイトル', $イシュータイトル, PDO::PARAM_STR);
+                    $stmt->bindParam(':ラベル', $ラベル, PDO::PARAM_STR);
+                    $stmt->bindParam(':優先順位', $優先順位, PDO::PARAM_STR);
+                    $stmt->bindParam(':イシューコミットID', $イシューコミットID, PDO::PARAM_STR);
+                    
+                    $stmt->execute();
+
+                    $stmt = $pdo->prepare("INSERT INTO repos (username, reponame) VALUES (:ユーザ名, :レポジトリ名)");
+
+                    $stmt->bindParam(':ユーザ名', $ユーザ名, PDO::PARAM_STR);
+                    $stmt->bindParam(':レポジトリ名', $レポジトリ名, PDO::PARAM_STR);
+                    
+                    $stmt->execute();
+
+                }
+                catch (PDOException $e) {
+                    echo "Connection failed: " . $e->getMessage();
+                }
                 
-                $ユーザ名 = $_POST['ユーザ名'];
-                $レポジトリ名 = $_POST['レポジトリ名'];
-                $イシュータイトル = $_POST['イシュータイトル'];
-                $ラベル = $_POST['ラベル'];
-                $優先順位 = $_POST['優先順位'];
-                $担当者 = $_POST['担当者'];
-                $イシューコミットID = $_POST['イシューコミットID'];
-
-
-                $stmt = $pdo->prepare("INSERT INTO issues (title,label,priority,issue_commit) VALUES (:イシュータイトル, :ラベル, :優先順位, :イシューコミットID)");
-                
-                $stmt->bindParam(':イシュータイトル', $イシュータイトル, PDO::PARAM_STR);
-                $stmt->bindParam(':ラベル', $ラベル, PDO::PARAM_STR);
-                $stmt->bindParam(':優先順位', $優先順位, PDO::PARAM_STR);
-                $stmt->bindParam(':イシューコミットID', $イシューコミットID, PDO::PARAM_STR);
-                
-                $stmt->execute();
-
-                $stmt = $pdo->prepare("INSERT INTO repos (username, reponame) VALUES (:ユーザ名, :レポジトリ名)");
-
-                $stmt->bindParam(':ユーザ名', $ユーザ名, PDO::PARAM_STR);
-                $stmt->bindParam(':レポジトリ名', $レポジトリ名, PDO::PARAM_STR);
-                
-                $stmt->execute();
                 
 
 
 
 
-                
+
             ?>
         
 
