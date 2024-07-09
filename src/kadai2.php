a727eaa491cacef76e6032942628f1883c94b638
ss21074 <wildsan-21455@outlook.jp>
2024-07-09 15:56:38 +0900

    kadai2.php

--- src/kadai2.php
+++ src/kadai2.php
@@ -4,10 +4,27 @@
         <title>イシュー管理システム</title>      
     </head>
     <body>
-        
-
-    
-
+        <form action = "listgen.php" method = "post" >
+            <br>
+            <label>ユーザ名:</label>
+            <input type="text" name="ユーザ名" size=5>
+            <label>レポジトリ名:</label>
+            <input type="text" name="レポジトリ名" size=5>
+            <br>
+            <label>イシュータイトル:</label>
+            <input type="text" name="イシュータイトル" size=5>
+            <label>ラベル:</label>
+            <select name="ラベル">
+                <option> バグ</option>
+                <option> 機能要求</option>
+            </select>
+            <input type="text" name="優先順位" size=5>
+            <label>担当者:</label>
+            <input type="text" name="担当者" size=5>
+            <label>イシューコミットID:</label>
+            <input type="text" name="イシューコミットID" size=5>
+            <br>
+        </from>
     
 
     </body>
