d78ac3c8aad93a4a7b68b09e1f45d2f8b7ed5593
ss21074 <wildsan-21455@outlook.jp>
2024-07-16 14:59:16 +0900

    表表示小泉

--- src/kadai2.php
+++ src/kadai2.php
@@ -49,7 +49,8 @@
                     $priority = $_POST['priority'];
                     $ID = $_POST['ID'];
                     
-                    if($user != null && $repo != null && $title != null && $label != null && $priority != null && $ID != null){
+                    if($user != NULL && $repo != NULL && $title != NULL && $label != NULL && $priority != NULL && $ID != NULL){
+                        echo "GFGCDRTDRTFCVGHCGJC";
                         $stmt_issues = $pdo->prepare("INSERT INTO issues (title, label, priority, issue_id) VALUES (:title, :label, :priority, :ID)");
                         $stmt_issues->bindParam(':title', $title);
                         $stmt_issues->bindParam(':label', $label);
